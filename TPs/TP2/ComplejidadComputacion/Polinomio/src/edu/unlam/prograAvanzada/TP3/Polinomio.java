ffad34b09e6e3934be36617f4175a32c34aebc3a
Maximiliano Federico Manso <maxifede20@gmail.com>
2018-10-16 00:52:35 -0300

    TP2_Recursiva
    Se sube las potencias recursivas, ambas. Pero hay una que no funciona.

--- TPs/TP2/ComplejidadComputacion/Polinomio/src/edu/unlam/prograAvanzada/TP3/Polinomio.java
+++ TPs/TP2/ComplejidadComputacion/Polinomio/src/edu/unlam/prograAvanzada/TP3/Polinomio.java
@@ -4,28 +4,59 @@ public class Polinomio {
 
 	private int grado;
 	private double[] coeficiente;
+
 	public Polinomio(int grado, double[] coeficiente) {
 		this.grado = grado;
 		this.coeficiente = coeficiente;
 	}
+
+	// calculo del polinomio para un valor en especifico
 	public double evaluarMsucesivas(double x) {
 		double resultado = 0;
-		
-		for(int i = this.grado; i > 0; i--) {
-			resultado += potenciaMSucesivas(this.coeficiente[this.grado - i], i);
+
+		for (int i = this.grado; i >= 0; i--) {
+			resultado += this.coeficiente[this.grado - i] * potenciaMSucesivas(x, i);
 		}
-		
-		return resultado ;
+
+		return resultado;
 	}
-	
-	public double potenciaMSucesivas(double x, int n) {
-		double resultado = 1;
-		for(int i = 0; i < n; i++) {
-			resultado = resultado * x;
+
+	public double evaluarRecursiva(double x) {
+		double resultado = 0;
+
+		for (int i = this.grado; i >= 0; i--) {
+			resultado += this.coeficiente[this.grado - i] * potenciaRecursiva(x, i);
 		}
-		if(n%2 == 0 && resultado < 0){
-			return resultado * -1;
+
+		return resultado;
+	}
+
+	// Calculos de las potecias de distintas formas
+
+	public double potenciaMSucesivas(double base, int exp) {
+		double resultado = 1;
+		for (int i = 0; i < exp; i++) {
+			resultado = resultado * base;
 		}
 		return resultado;
 	}
+
+	public double potenciaRecursiva(double base, int exp) {
+		if (exp == 0) {
+			return 1;
+		}
+		return base * potenciaRecursiva(base, exp - 1);
+	}
+	
+	public double potenciaRecursivaPar(double base, int exp) {
+		if (exp == 0) {
+			return 1;
+		}
+		if(exp%2 == 0) {
+			return base * potenciaRecursiva(base*base, exp/2);
+		}
+		return base * potenciaRecursiva(base, exp - 1);
+	}
+	
+	
 }
